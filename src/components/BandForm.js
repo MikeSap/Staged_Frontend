74b0a1af6f9ef5dd88ed443abb729222661af355
mike <mikesapienzaaudio@gmail.com>
2020-12-24 08:48:56 -0600

    api url fixed

--- src/components/BandForm.js
+++ src/components/BandForm.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux'
 import { editBand, clearBandErrors, newBand } from '../actions/Bands'
 import { useHistory } from "react-router";
-import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
